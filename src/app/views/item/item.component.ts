44cf2dfd7ecd3b6ec19860f78b15c49e4afe5321
BITHIN <bithin.softronet@gmail.com>
2022-03-06 22:13:28 +0530

    update

--- src/app/views/item/item.component.ts
+++ src/app/views/item/item.component.ts
@@ -20,6 +20,8 @@ export class ItemComponent implements OnInit {
   public itemFormDetails: any = {};
   public addEditItemModalRef: any;
   public isEdit: boolean = false;
+  public isPosterAdded: boolean = false;
+  public imageFile: any;
   page: number = 1;
   pageSize: number = 20;
   filterForm: any = {
@@ -101,6 +103,7 @@ export class ItemComponent implements OnInit {
       languageId: ''
     };
     this.isEdit = false;
+    this.isPosterAdded = false;
     this.subCategoryList = await this.getSubCategoryList();
     this.languageList = await this.getLanguageList();
     this.addEditItemModalRef = this.modalService.open(template, { size: 'lg', centered: true, backdrop: 'static' });
@@ -108,7 +111,11 @@ export class ItemComponent implements OnInit {
 
   async openEditItemModal(template: TemplateRef<any>, obj) {
     this.itemFormDetails = { ...obj };
+    if (this.itemFormDetails.viewItemPoster[0]) {
+      this.itemFormDetails.poster = environment.API_ENDPOINT + this.itemFormDetails.viewItemPoster[0].posterURL.replaceAll('\\', '/');
+    }
     this.isEdit = true;
+    this.isPosterAdded = true;
     this.subCategoryList = await this.getSubCategoryList();
     this.languageList = await this.getLanguageList();
     this.addEditItemModalRef = this.modalService.open(template, { size: 'lg', centered: true, backdrop: 'static' });
@@ -132,12 +139,16 @@ export class ItemComponent implements OnInit {
       this.toastr.warning('Please enter Item title', 'Warning');
       return;
     }
+    if (!this.isPosterAdded) {
+      this.toastr.warning('Please upload poster', 'Warning');
+      return;
+    }
     let url = `ViewItem`;
     // this.spinnerService.show();
     this.webService.post(url, this.itemFormDetails).subscribe((response: any) => {
-      this.getItemList();
-      this.toastr.success('Item added successfully', 'Success');
       this.addEditItemModalRef.close();
+      this.uploadPoster(response.viewItemId, false);
+      this.toastr.success('Item added successfully', 'Success');
     }, (error) => {
       console.log('error', error);
     });
@@ -161,12 +172,21 @@ export class ItemComponent implements OnInit {
       this.toastr.warning('Please enter Item title', 'Warning');
       return;
     }
-    let url = `ViewItem?id=${this.itemFormDetails.id}`;
+    if (!this.isPosterAdded) {
+      this.toastr.warning('Please upload poster', 'Warning');
+      return;
+    }
+    let url = `ViewItem?id=${this.itemFormDetails.viewItemId}`;
     // this.spinnerService.show();
     this.webService.put(url, this.itemFormDetails).subscribe((response: any) => {
-      this.getItemList();
-      this.toastr.success('Item updated successfully', 'Success');
-      this.addEditItemModalRef.close();
+      if (this.imageFile) {
+        this.uploadPoster(this.itemFormDetails.viewItemId, true);
+      } else {
+        this.getItemList();
+        this.toastr.success('Item updated successfully', 'Success');
+        this.addEditItemModalRef.close();
+      }
+
     }, (error) => {
       console.log('error', error);
     });
@@ -176,7 +196,7 @@ export class ItemComponent implements OnInit {
     this.confirmationDialogService.confirm('Delete', `Do you want to delete Item  ${obj.name}?`)
       .then((confirmed) => {
         if (confirmed) {
-          let url = `ViewItem?viewItemId=${obj.viewItemId}`;
+          let url = `ViewItem?viewItemId=${obj.viewItemId}&isActiveOrDeletes=true&isActiveOrDelete=Delete`;
           // this.spinnerService.show();
           this.webService.delete(url).subscribe((response: any) => {
             // this.spinnerService.hide();
@@ -197,6 +217,72 @@ export class ItemComponent implements OnInit {
         }
       })
       .catch((error) => { });
+  }
+
+
+  uploadItem(files: FileList, type: any) {
+    if (type == 'poster') {
+      this.itemFormDetails.poster = {};
+    }
+    let validation = this.validatePhotoUpload(files.item(0).name);
+    if (validation) {
+      this.imageFile = files.item(0);
+      this.getBase64(files.item(0), type);
+      // this.formDetails.logo.hasImg = true;
+    } else {
+      this.toastr.error("Please upload only JPG, PNG, GIF format", "Error");
+    }
+  }
+
+  getBase64(file, type) {
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      console.log('url', reader.result);
+      this.isPosterAdded = true;
+      this.itemFormDetails.poster = reader.result;
+
+    };
+    reader.onerror = function (error) {
+      console.log("Error: ", error);
+    };
+  }
+
+  uploadPoster(viewItemId, isDataUpdate) {
+    let url = `Poster/ViewItemPoster`;
+    var formData = new FormData();
+    formData.append('image', this.imageFile);
+    formData.append('ViewItemId', viewItemId);
+    this.webService.fileUpload(url, formData).subscribe((response: any) => {
+      this.getItemList();
+      //  this.spinnerService.hide();
+      //  this.addEditModalRef.close();
+      this.itemFormDetails.poster = environment.API_ENDPOINT + response.posterURL.replaceAll('\\', '/');
+      this.isPosterAdded = true;
+      if (isDataUpdate) {
+        this.getItemList();
+        this.toastr.success('Item updated successfully', 'Success');
+        this.addEditItemModalRef.close();
+      } else {
+        this.toastr.success('Poster uploaded', 'Success');
+      }
+
+
+    }, (error) => {
+      console.log('error ts: ', error);
+    });
+  }
+
+
+  validatePhotoUpload(fileName) {
+    var allowed_extensions = new Array("jpg", "jpeg", "png", "gif");
+    var file_extension = fileName.split(".").pop().toLowerCase(); // split function will split the filename by dot(.), and pop function will pop the last element from the array which will give you the extension as well. If there will be no extension then it will return the filename.
+    for (var i = 0; i <= allowed_extensions.length; i++) {
+      if (allowed_extensions[i] == file_extension) {
+        return true; // valid file extension
+      }
+    }
+    return false;
   }
 
 
